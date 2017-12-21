a4995fbb0ca9c2fee58cb3efb1ce518abc0f2b9a
Luay <luayghazwan@yahoo.com>
2017-12-20 16:05:11 -0800

    Changed datatypes and cards counter for every player

--- WarCardGame/WarCardGame/Program.cs
+++ WarCardGame/WarCardGame/Program.cs
@@ -5,32 +5,51 @@ namespace WarCardGame
 {
     public static class Game
     {
-        public static string[] CardDeck = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
-        public static string[] CardSuit = {"hearts", "diamonds", "clubs", "spades"};
+        public static int CardsLeftPlayerOne = 52;
+        public static int CardsLeftPlayerTwo = 52;
+
+        public static string[] CardDeck = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A" };
+        public static string[] CardSuit = { "hearts", "diamonds", "clubs", "spades" };
 
         public static string CheckCards(string playerOneCard, string playerOneSuit, 
                                         string playerTwoCard, string playerTwoSuit)
         {
-            var cardOne = Array.IndexOf(CardDeck, playerOneCard.ToUpper());
-            var suitOne = Array.IndexOf(CardSuit, playerOneSuit.ToLower());
+            int cardOne = Array.IndexOf(CardDeck, playerOneCard.ToUpper());
+            int suitOne = Array.IndexOf(CardSuit, playerOneSuit.ToLower());
 
-            var cardTwo = Array.IndexOf(CardDeck, playerTwoCard.ToUpper());
-            var suitTwo = Array.IndexOf(CardSuit, playerTwoSuit.ToLower());
+            int cardTwo = Array.IndexOf(CardDeck, playerTwoCard.ToUpper());
+            int suitTwo = Array.IndexOf(CardSuit, playerTwoSuit.ToLower());
 
             if (cardOne > cardTwo)
             {
+                CardsLeftPlayerOne++;
+                CardsLeftPlayerTwo--;
+
                 return "P1";
             }
             if (cardTwo > cardOne)
             {
+                CardsLeftPlayerOne++;
+                CardsLeftPlayerTwo--;
+
                 return "P2";
             }
-            return suitOne > suitTwo ? "P1" : "P2";
+
+            if (suitOne > suitTwo)
+            {
+                CardsLeftPlayerOne += 2;
+                CardsLeftPlayerTwo -= 2;
+                return "P1";
+            }
+
+            CardsLeftPlayerOne -= 2;
+            CardsLeftPlayerTwo += 2;
+            return "P2";
         }
 
         public static void Main(string[] args)
         {
-            
+
         }
     }
 }
