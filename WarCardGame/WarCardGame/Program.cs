0a721af7281d33744ae925b9fe39ec7be029795c
Luay <luayghazwan@yahoo.com>
2017-12-20 21:26:04 -0800

    Comments added to methods and test cases

--- WarCardGame/WarCardGame/Program.cs
+++ WarCardGame/WarCardGame/Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Linq;
 
 namespace WarCardGame
 {
     public static class Game
     {
+        // Giving 52 Cards to each player at the beginning of the Game
         public static int CardsLeftPlayerOne = 52;
         public static int CardsLeftPlayerTwo = 52;
 
+        // Card deck sorted from 2 to Ace
         public static string[] CardDeck = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A" };
+        // Card suit types valued in an ascendingly order
         public static string[] CardSuit = { "hearts", "diamonds", "clubs", "spades" };
 
+        /// <summary>
+        /// Randomize a card with Value and Suit then returns it as an array of 2.
+        /// </summary>
+        /// <returns>Return int array.</returns>
         public static int[] RandomCard()
         {
             Random random = new Random();
@@ -20,15 +26,46 @@ namespace WarCardGame
             return new[] { randomCard, randomSuit };
         }
 
+        // When two players match their Cards value a war happens to check for their Suits value
+        public static string War()
+        {
+            // Drawing two cards every war for Player One - Game rules
+            int[] playerOneRandomOne = RandomCard();
+            int[] playerOneRandomTwo = RandomCard();
+            int suitOne = playerOneRandomTwo[1];
+
+            // Drawing two cards every war for Player Two - Game rules
+            int[] playerTwoRandomOne = RandomCard();
+            int[] playerTwoRandomTwo = RandomCard();
+            int suitTwo = playerTwoRandomTwo[1];
+
+            // Increase cards number by 2 for P1 and return as winner - Decrease P2 cards number by 2 
+            if (suitOne > suitTwo)
+            {
+                CardsLeftPlayerOne += 2;
+                CardsLeftPlayerTwo -= 2;
+                return "P1";
+            }
+
+            // Increase cards number by 2 for P2 and return as winner - Decrease P1 cards number by 2 
+            CardsLeftPlayerTwo += 2;
+            CardsLeftPlayerOne -= 2;
+            return "P2";
+        }
+
+        /// <summary>
+        /// Battling between two players and returning who takes both cards (A war might happen).
+        /// </summary>
+        /// <param name="playerOne"></param>
+        /// <param name="playerTwo"></param>
+        /// <returns>Return Winner of the current Battle.</returns>
         public static string CheckCards(string[] playerOne, string[] playerTwo)
         {
-            string a = playerOne[0];
+            // Getting indices from Card's number and suit for later comparison
             int cardOne = Array.FindIndex(CardDeck, c => c == playerOne[0]);
-            int suitOne = Array.FindIndex(CardSuit, s => s == playerOne[1]);
-
             int cardTwo = Array.FindIndex(CardDeck, c => c == playerTwo[0]);
-            int suitTwo = Array.FindIndex(CardSuit, s => s == playerTwo[1]);
 
+            // Card one is bigger than Card two - increment Player one stack of cards
             if (cardOne > cardTwo)
             {
                 CardsLeftPlayerOne++;
@@ -37,6 +74,7 @@ namespace WarCardGame
                 return "P1";
             }
 
+            // Card one is bigger than Card two - increment Player two stack of cards
             if (cardTwo > cardOne)
             {
                 CardsLeftPlayerTwo++; 
@@ -45,37 +83,37 @@ namespace WarCardGame
                 return "P2";
             }
 
-            if (suitOne > suitTwo)
-            {
-                Console.WriteLine("----------------------- WAAAAAAAAR! --------------------------");
-                CardsLeftPlayerOne += 2;
-                CardsLeftPlayerTwo -= 2;
-                return "P1";
-            }
-
-            Console.WriteLine("----------------------- WAAAAAAAAR! --------------------------");
-            CardsLeftPlayerOne -= 2;
-            CardsLeftPlayerTwo += 2;
-            return "P2";
+            Console.WriteLine("\n---------------------------------- WAAAAAAAAR! ----------------------------------\n");
+            
+            // Declare War when two cards match values and return the winner
+            return War();
         }
 
+        // Both players draw once from their cards deck
         public static void Draw()
         {
+            // Generating random cards
             int[] randomCard1 = RandomCard();
             int[] randomCard2 = RandomCard();
 
+            // Construct two players with Card Value and Suit
             string[] playerOne = new string[] {CardDeck[randomCard1[0]], CardSuit[randomCard1[1]]};
             string[] playerTwo = new string[] {CardDeck[randomCard2[0]], CardSuit[randomCard2[1]]};
 
+            // LIVE results of both players
             Console.WriteLine("  P1: Cards Left ({0})   --->   {1} {2} VS {3} {4}   <---   P2: Cards Left ({5})",
                                 CardsLeftPlayerOne, playerOne[0], playerOne[1],
                                 playerTwo[0], playerTwo[1], CardsLeftPlayerTwo);
 
+            // Checking Players cards and returning the winner from every draw
             CheckCards(playerOne, playerTwo);
 
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Start game when the user runs the application. This method will check for cards left in every player's deck and draw new cards. 
+        /// </summary>
         public static void StartGame()
         {
             Console.WriteLine("\n----------------------- The game has just started! --------------------------" +
@@ -83,6 +121,7 @@ namespace WarCardGame
                               "\n---------------- CLICK ENTER TO DRAW A CARD FOR BOTH PLAYERS ---------------- ");
             Console.ReadLine();
 
+            // Checking if any player has ran out of cards to end the game
             while (true)
             {
                 if (CardsLeftPlayerOne == 0)
@@ -96,6 +135,7 @@ namespace WarCardGame
                     break;
                 }
 
+                // Making a Draw for new cards
                 Draw();
             }
 
@@ -103,6 +143,10 @@ namespace WarCardGame
             Console.Read();
         }
 
+        /// <summary>
+        /// Main Entry point for WarCardGame.cs
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             StartGame();
