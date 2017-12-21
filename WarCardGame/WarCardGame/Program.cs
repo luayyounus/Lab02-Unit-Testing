4d926471242499dbb429fb3f588056dac289faa8
Luay <luayghazwan@yahoo.com>
2017-12-20 20:03:54 -0800

    finished the game implementation with Points tally

--- WarCardGame/WarCardGame/Program.cs
+++ WarCardGame/WarCardGame/Program.cs
@@ -20,14 +20,14 @@ namespace WarCardGame
             return new[] { randomCard, randomSuit };
         }
 
-        public static string CheckCards(string playerOneCard, string playerOneSuit, 
-                                        string playerTwoCard, string playerTwoSuit)
+        public static string CheckCards(string[] playerOne, string[] playerTwo)
         {
-            int cardOne = Array.IndexOf(CardDeck, playerOneCard.ToUpper());
-            int suitOne = Array.IndexOf(CardSuit, playerOneSuit.ToLower());
+            string a = playerOne[0];
+            int cardOne = Array.FindIndex(CardDeck, c => c == playerOne[0]);
+            int suitOne = Array.FindIndex(CardSuit, s => s == playerOne[1]);
 
-            int cardTwo = Array.IndexOf(CardDeck, playerTwoCard.ToUpper());
-            int suitTwo = Array.IndexOf(CardSuit, playerTwoSuit.ToLower());
+            int cardTwo = Array.FindIndex(CardDeck, c => c == playerTwo[0]);
+            int suitTwo = Array.FindIndex(CardSuit, s => s == playerTwo[1]);
 
             if (cardOne > cardTwo)
             {
@@ -36,29 +36,76 @@ namespace WarCardGame
 
                 return "P1";
             }
+
             if (cardTwo > cardOne)
             {
-                CardsLeftPlayerOne++;
-                CardsLeftPlayerTwo--;
+                CardsLeftPlayerTwo++; 
+                CardsLeftPlayerOne--;
 
                 return "P2";
             }
 
             if (suitOne > suitTwo)
             {
+                Console.WriteLine("----------------------- WAAAAAAAAR! --------------------------");
                 CardsLeftPlayerOne += 2;
                 CardsLeftPlayerTwo -= 2;
                 return "P1";
             }
 
+            Console.WriteLine("----------------------- WAAAAAAAAR! --------------------------");
             CardsLeftPlayerOne -= 2;
             CardsLeftPlayerTwo += 2;
             return "P2";
         }
 
+        public static void Draw()
+        {
+            int[] randomCard1 = RandomCard();
+            int[] randomCard2 = RandomCard();
+
+            string[] playerOne = new string[] {CardDeck[randomCard1[0]], CardSuit[randomCard1[1]]};
+            string[] playerTwo = new string[] {CardDeck[randomCard2[0]], CardSuit[randomCard2[1]]};
+
+            Console.WriteLine("  P1: Cards Left ({0})   --->   {1} {2} VS {3} {4}   <---   P2: Cards Left ({5})",
+                                CardsLeftPlayerOne, playerOne[0], playerOne[1],
+                                playerTwo[0], playerTwo[1], CardsLeftPlayerTwo);
+
+            CheckCards(playerOne, playerTwo);
+
+            Console.ReadLine();
+        }
+
+        public static void StartGame()
+        {
+            Console.WriteLine("\n----------------------- The game has just started! --------------------------" +
+                              "\nEach player has 52 Cards, the first one who runs out of Cards loses the game" +
+                              "\n---------------- CLICK ENTER TO DRAW A CARD FOR BOTH PLAYERS ---------------- ");
+            Console.ReadLine();
+
+            while (true)
+            {
+                if (CardsLeftPlayerOne == 0)
+                {
+                    Console.WriteLine("Player Two Won!");
+                    break;
+                }
+                if(CardsLeftPlayerTwo == 0)
+                {
+                    Console.WriteLine("Player One Won!");
+                    break;
+                }
+
+                Draw();
+            }
+
+            Console.WriteLine("Thanks for watching two random players playing WAR!");
+            Console.Read();
+        }
+
         public static void Main(string[] args)
         {
-
+            StartGame();
         }
     }
 }
